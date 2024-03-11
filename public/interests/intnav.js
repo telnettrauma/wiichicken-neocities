77d9f743666a79634bd1c227fa4fc9e00534db99
wiichicken <50886841+wiichicken@users.noreply.github.com>
2024-03-11 14:51:54 -0400

    the navbar is lookin a little... blurry

---
+++ public/interests/intnav.js
@@ -0,0 +1,22 @@
+function flipTheScript() {
+	document.getElementById('poopBlock').innerHTML = '<div id="inHead"><div class="topLinks"><a href="/interests/music/index.html">MUSiC</a></div><a id="fakeLogo" href="/index.html"><div class="square" id="s1" style="background-color: red;"></div><div class="square" id="s2" style="background-color: #00a2ff; margin-right: 0;"></div></a><div class="topLinks"><a href="/interests/websites/index.html">WEBSiTES</a></div><style>.headerVisible {transform: translateY(0px) !important;}</style></div>';
+	document.getElementById('sideButton').innerHTML = '<div class="rect" style="background-color: red;"></div><div class="rect" style="background-color: #00a2ff;"></div><div class="rect" style="margin-bottom: 0;"></div>';
+	document.getElementById('overSide').innerHTML = '	<div id="closeSide" onclick="closeSidey()"><div class="rect" id="closeRect1"></div><div class="rect" id="closeRect2"></div><div class="rect" id="closeRect3"></div></div><a href="/index.html">HOME</a><a href="/aboutme.html">ABOUT ME</a><a href="/creative/index.html">ART</a><a href="/interests/index.html">MEDiA</a><a href="/more/index.html">MORE</a><a href="/sitemap.html" id="bottomNavLink">SiTEMAP</a>';
+}
+function openSidey() {document.getElementById('overSide').style.display = 'flex';}
+function closeSidey() {document.getElementById('overSide').style.display = 'none';}
+flipTheScript();
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));}
+var whatIsOpera = (navigator.userAgent.match(/Opera|OPR\//) ? true : false);
+async function banOpera() {
+	if (whatIsOpera === true) {
+		await sleep(100);
+		document.body.innerHTML = '<h1>Oops!</h1><p>Opera browsers are not supported. For the best, most private browsers, check these out:</p><br><ul><li><a href="https://qutebrowser.org">Qute Browser</a></li><li><a href="https://www.apple.com/safari/">Safari</li></ul><br><br><p>Or... look up how to fake your UA. Either works.</p>';
+	}
+}
+banOpera();
+window.addEventListener('scroll', () => {
+	const chewTooth = document.getElementById('chewTooth');
+	const scrollPosition = window.scrollY;
+	chewTooth.style.backgroundPositionY = `${scrollPosition * -0.2}px`;
+});
