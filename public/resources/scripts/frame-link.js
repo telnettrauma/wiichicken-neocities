13fdd0128ee36d080b740dd497d632bd8f077dd1
wiichicken <50886841+wiichicken@users.noreply.github.com>
2024-06-04 13:23:34 -0400

    updated frame-link to make URLs shorter

--- public/resources/scripts/frame-link.js
+++ public/resources/scripts/frame-link.js
@@ -4,13 +4,13 @@
 // Optional: add 'updateTitle = false;' if you want to disable title updating. (Default is true)
 // Optional: add 'titlePrefix = "My Site ";' if you want to add a prefix to your titles. (Default is none)
 // Optional: add 'pageParam = "z";' if you want to change the url path of your pages. (Default is z)
+// Optional: add 'basePath - "path";' to set a base path
 
 var mainFrame;
 var firstLoad = true;
 var updateTitle = true;
 var pageParam = "z";
 var titlePrefix = "";
-var hitCounterFunction = undefined;
 
 //Event to handle first page load - Also sets up the mainFrame
 window.addEventListener("DOMContentLoaded", (e) => {
@@ -31,28 +31,38 @@ function setMainFrame() {
 	let params = new URLSearchParams(window.location.search);
 	let page = params.get(pageParam);
 	if (page != null) {
-		mainFrame.src = page;
+		if (typeof basePath !== 'undefined') {
+			mainFrame.src = basePath + page;
+		} else {
+			mainFrame.src = page;
+		}
 	}
 }
 
 //Updates the browser history with the current page, causing the URL bar to update.
 async function updateHistory() {
+	let fullPath = mainFrame.contentWindow.location.pathname;
+	let page = fullPath;
+
+	if (typeof basePath !== 'undefined') {
+		if (fullPath.startsWith(basePath)) {
+			page = fullPath.replace(basePath, "");
+		}
+	}
+
 	let title = titlePrefix + mainFrame.contentDocument.title;
-	
-	// Stops the page getting into an infinate loop reloading itself.
+
+	// Stops the page getting into an infinite loop reloading itself.
 	if (firstLoad) {
 		firstLoad = false;
 		if (updateTitle) {
 			document.title = title;
 		}
-		if (hitCounterFunction != undefined) {
-			hitCounterFunction();
-		}
 		return;
 	}
-	
-	history.replaceState({}, title, "?" + pageParam + "=" + mainFrame.contentWindow.location.pathname);
-	
+
+	history.replaceState({}, title, "?" + pageParam + "=" + page);
+
 	if (updateTitle) {
 		document.title = title;
 	}
