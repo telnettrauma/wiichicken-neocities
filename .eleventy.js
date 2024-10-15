0ba61eb7af98904d278a293ee2a36fcbf6b72231
wiichicken <50886841+wiichicken@users.noreply.github.com>
2024-10-15 18:22:34 -0400

    11ty links automatically have target=_blank

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
 
 module.exports = function (eleventyConfig) {
 
@@ -6,6 +7,24 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("js");
 
+	let markdownLib = markdownIt({
+		html: true,
+		linkify: true
+	}).use(function(md) {
+		// override link_open rule to add target="_blank"
+		const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+			return self.renderToken(tokens, idx, options);
+		};
+
+		md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+			const aIndex = tokens[idx].attrIndex('target');
+
+			if (aIndex < 0) {tokens[idx].attrPush(['target', '_blank']);} else {tokens[idx].attrs[aIndex][1] = '_blank';}
+
+			return defaultRender(tokens, idx, options, env, self);
+		};
+	});
+	eleventyConfig.setLibrary("md", markdownLib);
 	eleventyConfig.addFilter("date", (dateObj, format = "MMMM d, yyyy") => {
 		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
 	});
